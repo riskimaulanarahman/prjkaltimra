3833c1b7d504903aeca8f204ba38f875c2ed02ee
Pinday App <pindayapp@gmail.com>
2023-08-19 18:56:42 +0800

    add portal

--- resources/views/includes/partials/announcements.blade.php
+++ resources/views/includes/partials/announcements.blade.php
@@ -1,7 +1,8 @@
 @if($announcements->count())
     @foreach($announcements as $announcement)
         <x-utils.alert :type="$announcement->type" :dismissable="false" class="pt-1 pb-1 mb-0">
-            {{ (new \Illuminate\Support\HtmlString($announcement->message)) }}
+            {{-- {{ (new \Illuminate\Support\HtmlString($announcement->message)) }} --}}
+            <marquee>Portal Honda Kalimantan</marquee>
         </x-utils.alert>
     @endforeach
 @endif
